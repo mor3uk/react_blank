da4787521746956ce5b7b7f6c220a7193691b62e
mor3uk <oldegs2017@gmail.com>
2019-11-01 15:43:06 +0400

    webpack setup for prod

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import './styles/styles.scss';
 import 'normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
 
 const store = configureStore();
 
