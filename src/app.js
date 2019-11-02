fbfa8378ff76888c4c1f6e03ec427579f90c3f67
mor3uk <oldegs2017@gmail.com>
2019-11-02 13:48:47 +0400

    actions connected to db

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import './styles/styles.scss';
 import 'normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
+import { startSetExpenses } from './actions/expenses';
 
 const store = configureStore();
 
@@ -16,4 +17,8 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
